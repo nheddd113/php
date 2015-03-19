8a66bd50f7b8dba0ccd7dde85e1f02cd837ec9cc
nheddd113 <nheddd113@163.com>
2015-03-19 17:42:42 +0800

    删除

--- newidc/Sql/admin.sql
+++
@@ -1,8 +0,0 @@
-create table uqee_admin(
-uid int not null auto_increment,
-username varchar(30) not null ,
-password varchar(32) not null,
-createtime timestamp not null default now(),
-primary key uid,
-unique index uid_idx(uid) using btree
-)ENGINE=MyISAM  DEFAULT CHARSET=utf8;
